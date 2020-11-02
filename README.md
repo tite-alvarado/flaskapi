0d933d422caf80e9b680bb05af1bcd8206a80387
Felipe Alvarado <azuleonardo@gmail.com>
2020-11-01 22:08:58 -0600

    Introducing helm chart, minikube with helm installation

--- README.md
+++ README.md
@@ -1,44 +1,77 @@
 # Flask API
 
-## Small API to crypt/decrypt
+## Small codec API
 
 ### RUN IT:
 
 #### Flask Native
-`flask run`
-`sh tests.sh`
+```
+flask run
+sh tests.sh
+```
 
-#### With docker
-`docker-compose build && docker-compose up -d`
-`docker logs -f flaskapp`
+#### With docker, compose, swarm native
+```
+docker-compose build && docker-compose up -d
+```
+
+```
+docker swarm init ; docker stack deploy --compose-file docker-compose.yml flaskapp
+```
+
+```
+docker logs -f flaskapp
+```
 
 ##### TEAR DOWN:
-`docker-compose down`
+```
+docker-compose down
+```
+
+```
+docker stack rm flaskapp ; docker swarm leave --force
+```
 
 #### With Kubernetes
 
 Based on `minikube` and `helm`
 
-* Install for your distro, eg on OSX with brew `brew install minikube`
+* Install minikube for your distro, eg on OSX with brew `brew install minikube`
 * Install helm, eg `brew install helm`
 * `sh minik8s.sh` to setup a cluster and deploy locally built image
 
 ##### TEAR DOWN:
-`kubectl delete -f flaskapp-chartv1.yaml`
+```
+helm uninstall flaskapp
+minikube delete
+```
 
 ### TEST IT:
 
-* From pod itself
-`docker exec -ti <flaskapp> sh /app/tests.sh`
+From pod itself
 ```
-kubectl get pods | awk '/^flaskapp/ {print $1}' | xargs -I % kubectl exec --stdin --tty % -- /bin/sh /app/tests.sh 2>/dev/null
+docker exec -ti <flaskapp> sh /app/tests.sh
 ```
-* From pod in cluster
+```
+kubectl get pods | awk '/^flaskapp/ {print $1}' | \
+xargs -I % kubectl exec --stdin --tty % -- /bin/sh /app/tests.sh 2>/dev/null
+```
+From pod in cluster
 ```
 kubectl run -it --rm --tty --image=alpine:3.5 alpine-shell -- /bin/sh
 # / wget -q0- <flaskapp_cluster_ip>:5000/health
 ```
+From workstation with port-forwarding
+```
+kubectl port-forward deployment/flaskapp 5000 5000 &
+sh tests.sh
+```
 
 ### SEE LOGS:
-`docker logs -f flaskapp`
-`kubectl logs pod <flaskapp_pod>`
+```
+docker logs -f <flaskapp>
+```
+
+```
+kubectl logs pod <flaskapp_pod>
+```
--- minik8s.sh
+++ minik8s.sh
@@ -8,31 +8,42 @@ eval $(minikube -p minikube docker-env)
 minikube status
 kubectl config view
 kubectl get nodes
-kubectl get services
 kubectl get pods --all-namespaces
 
-# heml and compose registry support
-# kubectl create namespace compose
-# kubectl -n kube-system create serviceaccount tiller
-# kubectl -n kube-system create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount kube-system:tiller
-
-# helm init --service-account tiller
-helm install etcd-operator stable/etcd-operator --namespace compose
-
-helm repo add stable https://charts.helm.sh/stable
+# helm repo add stable https://charts.helm.sh/stable
 
 # build and expose
 docker-compose build
-kubectl apply -f flaskapp-chartv1.yaml
-# kubectl apply -f flaskapp-chartv1-service.yaml
-kubectl expose deployment flaskapp --protocol=TCP --port=5000 --name=flaskapp
+# kubectl apply -f flaskapp/templates/deployment.yaml
+# kubectl expose deployment/flaskapp --name=flaskapp
 
-# test pod from itself
+# install to k8s with helm
+helm lint flaskapp && \
+helm package flaskapp && \
+helm install --dry-run --debug flaskapp flaskapp-0.1.0.tgz && \
+helm install flaskapp flaskapp-0.1.0.tgz
+
 sleep 30 # docker start_period
-kubectl get pods | awk '/^flaskapp/ {print $1}' | xargs -I % kubectl exec --stdin --tty % -- /bin/sh /app/tests.sh 2>/dev/null
-# test from workstation to exposed port
-# kubectl run -it --rm --tty --image=alpine:3.5 alpine-shell -- /bin/sh
-# # / wget -q0- <flaskapp_cluster_ip>:5000/health
+echo ' >> sleep while starting ...'
 
-# sh tests.sh        # fails / private service
+helm list
+kubectl get deployments
+kubectl get services
+kubectl describe service flaskapp
+
+echo ' >> testing  ...'
+# test pod from itself
+kubectl get pods | awk '/^flaskapp/ {print $1}' | xargs -I % kubectl exec --stdin --tty % -- /bin/sh /app/tests.sh 2>/dev/null
+
+echo '
+
+  # for manual testing...
+  # test from pod in cluster
+  ## kubectl run -it --rm --tty --image=alpine:3.5 alpine-shell -- /bin/sh
+  ## / wget -q0- <flaskapp_cluster_ip>:5000/health
+
+  # test from workstation with port-forwarding
+  ## kubectl port-forward deployment/flaskapp 5000 5000 &
+  ## sh tests.sh
+'
 # minikube dashboard # optional
